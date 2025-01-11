8a156e955fba725c1b2736e28befa2f59bb4f90a
ManasaG20 <gmanasa20804@gmail.com>
2025-01-11 12:36:11 +0530

    updated notification table

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
-
 
 const dataControl = require('./control/controlData');
 
+app.use(cors({
+  origin: 'http://localhost:5173', // Change this to your frontend's URL if different
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 // Middleware to serve static files from the React `dist` folder
 app.use(express.json());
+
 app.get('/api/get-rides' , dataControl.getRide);
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
 // });
 // Catch-all route to serve `index.html` for any unmatched routes
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../','frontend','dist','index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../','frontend','dist','index.html'));
+// });
 
 
 app.post('/api/contact' , dataControl.postContactData);
 app.post('/api/user' , dataControl.createUser);
 app.post('/api/offer-ride' , dataControl.postRide);
+app.post('/api/booking' , dataControl.postBooking);
+app.post('/api/notification' , dataControl.postNotification);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
--- control/controlData.js
+++ control/controlData.js
@@ -29,9 +29,27 @@ exports.postRide = async (req, res) => {
     console.log(req.body);
     const {departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance , email} = req.body;
     try{
-      await db.execute('insert into ride(departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance,email) values(?,?,?,?,?,?,?,?,?,?,?)',[departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance , email]);
+      await db.execute('insert into ride(departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance,email,avail_seat) values(?,?,?,?,?,?,?,?,?,?,?,?)',[departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance , email , seats]);
   
-      console.log('Form submitted with data:', departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance);
+      console.log('Form submitted with data:', departure,destination,seats,price,date,time,vehicleType,vehicleDetails,instantBooking,distance,seats);
+      res.status(201).send('msg received');
+    }
+    catch(err){
+      console.log('database error: ',err.message);
+    }
+};
+
+exports.postBooking = async (req, res) => {
+    console.log(req.body);
+    const {r_email , p_email , seats , isInstant} = req.body;
+    try{
+      const [avail_seat] = await db.execute('select avail_seat from ride where email=?',[r_email]);
+      const bookedSeats = Math.min(seats , avail_seat[0]);
+      const status = isInstant?'accepted':'requested';
+      console.log(avail_seat , bookedSeats , status);
+      await db.execute('insert into booking(r_email , p_email , seats , status) values(?,?,?,?)',[r_email,p_email,bookedSeats,status]);
+      console.log('submitted data:', r_email,p_email,seats,isInstant);
+      await db.execute('update ride set avail_seat=? where email=?',[bookedSeats , r_email]);
       res.status(201).send('msg received');
     }
     catch(err){
@@ -47,15 +65,28 @@ exports.getRide = async (req, res) => {
   if (!to || !from || !date) {
     return res.status(400).send('Parameter is required');
   }
-
+  
   // const query = 'SELECT name,email,user_id from user';
-  const query = 'SELECT r.rid as id , r.departure as departure, r.destination as `to`,r.seats , r.price , r.time as departureTime , r.instantBooking,r.vehicleDetails as vehicle, u.name  FROM ride r inner join user u on u.email = r.email where r.departure = ? and r.destination = ? and r.date = ?';
+  const query = 'SELECT r.rid as id , r.departure as departure, r.destination as `to`,r.avail_seat as seats , r.price , r.time as departureTime , r.instantBooking as isInstant,r.date,r.vehicleDetails as vehicle,r.email, u.name  FROM ride r inner join user u on u.email = r.email where r.departure = ? and r.destination = ? and r.date = ?';
   try {
     const [results] = await db.execute(query ,[from ,to,date]);
     console.log('Results:', results);
     res.json(results);  // Send the fetched data as JSON response
   } catch (err) {
     console.error('Error fetching data:', err);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+exports.postNotification = async (req , res) => {
+  console.log(req.body);
+  const {email , title , msg } = req.body;
+  try{
+    await db.execute('insert into notification(email,title,msg,status) values(?,?,?,?)',[email,title,msg,'unread']); 
+    res.status(201).send('msg received');
+  }
+  catch(err){
+    console.log('error posting notification : ' ,err.message);
     res.status(500).send('Internal Server Error');
   }
 }
